cbe93ec5509b0eff2e7e81dcfc5f4824db2d5376
Lucas Binder <lbinder500@gmail.com>
2021-02-01 21:16:12 -0800

    track war attacks and update database with results

--- database/database.py
+++ database/database.py
@@ -1,5 +1,57 @@
-#TODO: create a database object for easy access to DB
+import mysql.connector
+
+from mysql.connector import errorcode
+from database.tokens import private
 
 class DataBase:
+    connection = None
     def __init__(self):
-        print("connect to db")
+        try:
+            self.connection = mysql.connector.connect(**private.config)
+        except mysql.connector.Error as e:
+            if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Error with username or password")
+            elif e.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(e)
+
+    def close_connection(self):
+        self.connection.close()
+
+    def execute(self, query, values):
+        cursor = self.connection.cursor()
+        cursor.execute(query, values)
+        self.connection.commit()
+        cursor.close()
+
+    def get_table(self, name):
+        query = "SELECT * FROM " + name
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        war_table = cursor.fetchall()
+        cursor.close()
+        return war_table
+
+    def insert_into_wars(self, values):
+        query = "INSERT INTO WARS (attackId, name, attackOneUsed, attackOneStars, attackOneDestruction, attackTwoUsed, attackTwoStars, attackTwoDestruction) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        self.execute(query, values)
+
+    def update_attacks(self, values, id):
+            query = "UPDATE wars SET attackOneUsed = %s, attackOneStars = %s, attackOneDestruction = %s, attackTwoUsed = %s, attackTwoStars = %s, attackTwoDestruction = %s WHERE attackID = %s"
+            self.execute(query, values+[id])
+
+    def entry_exists_war(self, attackID):
+        query = "select exists(select * from war where attackID = " + attackID 
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "SELECT attackID, COUNT(*) FROM wars WHERE attackID = %s GROUP BY attackID",
+            (attackID,)
+        )
+        results = cursor.fetchone()
+        if not results:
+            return False
+        else:
+            return True
+        
+    
--- database/manageData.py
+++ database/manageData.py
@@ -1,37 +1,79 @@
 import time
 
 from database.getData import *
+from database.database import *
 
-
+""" manage_wars
+    Controls the war state. Control flow for managing wars
+    happens here.
+"""
 def manage_wars():
+    db = DataBase()
+    new_war = True
     while True:
         war = get_current_war_information()
-        time.sleep(1)
-        if (war['state'] == 'preparation'):
-            add_new_war(war)
-        elif (war['state'] == 'attacking'):
-            track_war()
+        if (war['state'] == 'preparation' and new_war):
+            add_new_war(war, db)
+            new_war = False
 
+        while (war['state'] == 'inWar'):
+            war = get_current_war_information()
+            track_war(war, db)
+            time.sleep(10)
+        
+        new_war = True
+        time.sleep(10)
 
-# Only add if there isn't a war against this clan
-def add_new_war(war):
+""" add_new_war
+    war: dictionary of war data
+    db: database object
+    When a new war is detected this sets up the database for info
+    to be added
+"""
+def add_new_war(war, db):
     members = war['clan']['members']
     enemy_tag = war['opponent']['tag']
     for member in members:
         tag = member['tag']
-        print("adding " + str(tag) + " to war against " + enemy_tag)
+        name = member['name']
+        if not db.entry_exists_war(enemy_tag+tag):
+            values = [enemy_tag+tag, name, 'F', None, None, 'F', None, None]
+            db.insert_into_wars(values)
 
+"""track_war
+    war: dictionary of war data
+    db" database object
+    When a war is active this updates each players war stats.
+"""
+def track_war(war, db):
+    members = war['clan']['members']
+    enemy_tag = war['opponent']['tag']
+    for member in members:
+        if (member['name'] == 'Sire'):
+            print(member['attacks'])
+            print("-"*10)
+        values = parse_values(member)
+        if (values != None):
+            db.update_attacks(values, enemy_tag+member['tag'])
 
-def track_war():
-    active = True
-    while active:
-        war = get_current_war_information()
-        time.sleep(1)
-        if (war['state'] == 'notInWar'):
-            active = False
-        else:
-            members = war['clan']['members']
-            enemy_tag = war['opponent']['tag']
-            for member in members:
-                print("updating " + str(member['tag']) + " attacks in war against " + enemy_tag)
-
+""" parse_values
+    member: dictionary of member information
+    Finds how many attacks a member has completed and returns values
+    to update that member's information.
+"""
+def parse_values(member):
+    if 'attacks' in member:
+        if (len(member['attacks']) == 1):
+            values = ['T', 
+                      int(member['attacks'][0]['stars']),
+                      float(member['attacks'][0]['destructionPercentage']),
+                      'F', None, None]
+        elif (len(member['attacks']) == 2):
+            values = ['T', 
+                      int(member['attacks'][0]['stars']),
+                      float(member['attacks'][0]['destructionPercentage']),'T', 
+                      int(member['attacks'][1]['stars']),
+                      float(member['attacks'][1]['destructionPercentage'])]
+    else:
+        values = None
+    return values
