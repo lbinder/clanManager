664c6d98846757995f16cc7e2d2b1aeae7c22c7e
Lucas Binder <lbinder500@gmail.com>
2021-02-01 21:22:09 -0800

    update gitingore

--- database/tokens/private.py
+++ database/tokens/private.py
@@ -1,7 +1,10 @@
-bearer_token = ""
+clan_tag = "J2R9J8PC"
+bearer_th = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY2NmVkY2Y2LTIwMzMtNGNkNy04MGY4LWI4YWM1YzhkZDY0NyIsImlhdCI6MTYxMTg4NTQyNSwic3ViIjoiZGV2ZWxvcGVyLzAxNjQyOWQzLTIyMTItYjRmZi00MGY1LWZkMDFlYWFhMTJjYiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE3NC4yMS44NS4xODQiXSwidHlwZSI6ImNsaWVudCJ9XX0.QZo65-G6aFRPqwYU7sgmoR2fEqy60frm74QPh0Noe-48RzGosfPjYByIFoFkMiE7nmvYuBJ6fcOB0U8XOIWI5A"
+bearer_h = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM3ZTExODQxLTg4MmEtNGVmZS1hZTIxLTUxZDU0M2RkOTUyMCIsImlhdCI6MTYxMjE0OTM4NCwic3ViIjoiZGV2ZWxvcGVyLzAxNjQyOWQzLTIyMTItYjRmZi00MGY1LWZkMDFlYWFhMTJjYiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk3LjEyNi41Mi42OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.sDEo0wQUT-Sx9RoApTYjKeYNcRTUX6b5c_ebsCkAgLt6T_e3Wp1GlJTgxsen4OgjTMXfkyKYskqwwSBASVVo6A"
+
 config = {
-   'user': 'username',
-   'password': 'password',
-   'host': '127.0.0.1',
-   'database': 'db name'
+    'user': 'root',
+    'password': '!32lucas',
+    'host': '127.0.0.1',
+    'database': 'TeamFam'
 }
